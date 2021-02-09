0c640674cec893a64f677d577e5521f8fd469b0b
Darima Motta <darima_mtp@mail.ru>
2021-02-10 02:40:12 +0300

    conditionsandcycles  exercise

--- js/script.js
+++ js/script.js
@@ -8,10 +8,23 @@ const personalMovieDB={
     genres:[],
     private: false,
 };
+for (let i=0;i<2;i++){
 const a=prompt('Один из последних фильмов?',''),
- b=prompt('На сколько оцените его?',''),
- c=prompt('Один из последних фильмов?',''),
- d=prompt('На сколько оцените его?','');
+ b=prompt('На сколько оцените его?','');
+ if (a!=null&&b!=null&a!=''&&b!=''&&a.length<50){
+
+ 
+ 
 personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+console.log('done');}
+else {console.log('error');
+i--;}}
+if (personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмов");
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 )
+{console.log("Вы классический зритель");}
+else if (personalMovieDB.count > 30){ console.log("Вы киноман");}
+else {console.log("произошла ошибка");}
 console.log(personalMovieDB);
+ 
